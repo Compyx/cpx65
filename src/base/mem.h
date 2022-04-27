74bdb709189adb9cdad8f5cccf67ad28a9c3b2a9
compyx <b.wassink@ziggo.nl>
2022-04-27 14:59:49 +0200

    Add GPL v2 license notice to all sources files

--- src/base/mem.h
+++ src/base/mem.h
@@ -3,6 +3,25 @@
  * \ingroup base
  */
 
+/*
+cpx65 - Assembler for MOS 6502 and variants.
+Copyright (C) 2019-2022  Bas Wassink.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #ifndef BASE_MEM_H
 #define BASE_MEM_H
 
